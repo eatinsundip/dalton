14a1ca5d8578471306e313e7c79ed16ec354b06c
eatinsundip <43767555+eatinsundip@users.noreply.github.com>
2024-05-16 15:16:22 -0500

    Update start-dalton.sh
    Added docker and docker-compose install commands if I need them.

--- start-dalton.sh
+++ start-dalton.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# build the docker containers and start them up
+# Install Docker.io
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+#docker-compose --version
 
+# Install Docker-Compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+#docker-compose --version
+
+# build the docker containers and start them up
 cd "${0%/*}"
 docker-compose build && docker-compose up -d
